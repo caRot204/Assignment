28628b8d8cf4662543dc720f7767174e74ca5da5
Kakarot <105483542+caRot204@users.noreply.github.com>
2022-12-07 09:19:20 +0700

    api

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://project-hungthinhland-api-main-assignment-react-carot204.vercel.app/api/",
 
 })
 
